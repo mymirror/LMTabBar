3de3b1d305fc444abd4b01be2190a6d864134f1d
ponted <ponted@ponteddeMacBook-Pro.local>
2018-12-07 10:07:48 +0800

    add1

--- LMTabBar/LMTabBar/LMTabBar.m
+++ LMTabBar/LMTabBar/LMTabBar.m
@@ -11,12 +11,66 @@
 #import <LMTools/UIButton+Edge.h>
 
 @interface LMTabBar()
+{
+    UIEdgeInsets _oldSafeAreaInsets;
+}
 @property (nonatomic, strong) UIButton *middleBtn;
 @property (nonatomic, strong) UIButton *sendBtn;
 
 @end
 
 @implementation LMTabBar
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        _oldSafeAreaInsets = UIEdgeInsetsZero;
+    }
+    return self;
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+    _oldSafeAreaInsets = UIEdgeInsetsZero;
+}
+
+- (void)safeAreaInsetsDidChange {
+    [super safeAreaInsetsDidChange];
+    
+    if (!UIEdgeInsetsEqualToEdgeInsets(_oldSafeAreaInsets, self.safeAreaInsets)) {
+        [self invalidateIntrinsicContentSize];
+        
+        if (self.superview) {
+            [self.superview setNeedsLayout];
+            [self.superview layoutSubviews];
+        }
+    }
+}
+
+- (CGSize)sizeThatFits:(CGSize)size {
+    size = [super sizeThatFits:size];
+    
+    if (@available(iOS 11.0, *)) {
+        float bottomInset = self.safeAreaInsets.bottom;
+        if (bottomInset > 0 && size.height < 50 && (size.height + bottomInset < 90)) {
+            size.height += bottomInset;
+        }
+    }
+    
+    return size;
+}
+
+
+- (void)setFrame:(CGRect)frame {
+    if (self.superview) {
+        if (frame.origin.y + frame.size.height != self.superview.frame.size.height) {
+            frame.origin.y = self.superview.frame.size.height - frame.size.height;
+        }
+    }
+    [super setFrame:frame];
+}
 
 - (void)setMaxIndex:(NSInteger)maxIndex
 {
